7e27eeabe8ecd85b491c86d104097fbbfce57bcf
Kamrul Hasan <kamrulhasan20656@gmail.com>
2023-12-10 21:14:57 +0600

    express js is working

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-console.log("hello world");
-console.log("hello node js");
 
-const express = require('express')
+const express = require('express');
 const app = express()
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.send('hello world');
+})
+app.get('/profile',(req,res) =>{
+    res.send('Hello I am from profile')
+})
+app.listen(3000,()=>{
+    console.log("listen the post ")
 })
