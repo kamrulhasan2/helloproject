1cc59ddb30df5b6d5c397c9790ea57ce7684da83
Kamrul Hasan <kamrulhasan20656@gmail.com>
2023-12-10 19:32:41 +0600

    work with express js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 console.log("hello world");
 console.log("hello node js");
-alert("this is alert");
+
+const express = require('express')
+const app = express()
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
